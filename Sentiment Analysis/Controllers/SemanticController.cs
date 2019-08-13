e8bf8be02404058cd7692f6d6b65703633d36808
Habib ul Rehman <mhabibulrehman@gmail.com>
2019-08-13 18:10:01 +0500

    Add project files.

---
+++ Sentiment Analysis/Controllers/SemanticController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+using SentimentAnalysisML.Model.DataModels;
+
+namespace Sentiment_Analysis.Controllers
+{
+    public class SemanticController : Controller
+    {
+        [HttpGet]
+        public IActionResult Analysis()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Analysis(ModelInput input)
+        {
+            // Load the model
+            MLContext mlContext = new MLContext();
+
+            ITransformer mlModel = mlContext.Model.Load(@"..\Sentiment AnalysisML.Model\MLModel.zip", out var modelInputSchema);
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+            //Input
+            input.Year = DateTime.Now.Year;
+            // Try model on sample data
+            ModelOutput result = predEngine.Predict(input);
+            ViewBag.Result = result;
+            return View();
+        }
+    }
+}
---
+++ Sentiment Analysis/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sentiment_Analysis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
